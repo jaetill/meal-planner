8afc1afcbc9c00955668082123c0027fadb184f3
Jason Tilley <tilley_jason@hotmail.com>
2026-05-18 23:40:53 -0400

    feat(iac): Slice 5 (Secrets + Log groups) - Phase 6 complete
    tofu apply: 7 imported, 7 changed (tags), 0 destroyed. Secret meal-planner/secrets (values not in TF; rotate via aws CLI). 6 CloudWatch log groups (no retention set; ADR-0009 recommends 90d for prod - separate change).

--- terraform/envs/prod/imports.tf
+++ terraform/envs/prod/imports.tf
@@ -1,314 +1,34 @@
 import {
-  to = aws_api_gateway_rest_api.mp_rest
-  id = "e2h43o5aje"
+  to = aws_secretsmanager_secret.main
+  id = "arn:aws:secretsmanager:us-east-2:214599503944:secret:meal-planner/secrets-WnfBUU"
 }
 
 import {
-  to = aws_api_gateway_authorizer.mp_rest_cognitoauth
-  id = "e2h43o5aje/o0c2k9"
+  to = aws_cloudwatch_log_group.groups
+  id = "/aws/lambda/meal-planner-groups"
 }
 
 import {
-  to = aws_api_gateway_resource.mp_rest_groups
-  id = "e2h43o5aje/0dk12u"
+  to = aws_cloudwatch_log_group.kroger
+  id = "/aws/lambda/meal-planner-kroger"
 }
 
 import {
-  to = aws_api_gateway_method.mp_rest_groups_get
-  id = "e2h43o5aje/0dk12u/GET"
+  to = aws_cloudwatch_log_group.nutrition
+  id = "/aws/lambda/meal-planner-nutrition"
 }
 
 import {
-  to = aws_api_gateway_integration.mp_rest_groups_get
-  id = "e2h43o5aje/0dk12u/GET"
+  to = aws_cloudwatch_log_group.plan
+  id = "/aws/lambda/meal-planner-plan"
 }
 
 import {
-  to = aws_api_gateway_method.mp_rest_groups_options
-  id = "e2h43o5aje/0dk12u/OPTIONS"
+  to = aws_cloudwatch_log_group.share
+  id = "/aws/lambda/meal-planner-share"
 }
 
 import {
-  to = aws_api_gateway_integration.mp_rest_groups_options
-  id = "e2h43o5aje/0dk12u/OPTIONS"
-}
-
-import {
-  to = aws_api_gateway_method_response.mp_rest_groups_options_200
-  id = "e2h43o5aje/0dk12u/OPTIONS/200"
-}
-
-import {
-  to = aws_api_gateway_integration_response.mp_rest_groups_options_200
-  id = "e2h43o5aje/0dk12u/OPTIONS/200"
-}
-
-import {
-  to = aws_api_gateway_method.mp_rest_groups_post
-  id = "e2h43o5aje/0dk12u/POST"
-}
-
-import {
-  to = aws_api_gateway_integration.mp_rest_groups_post
-  id = "e2h43o5aje/0dk12u/POST"
-}
-
-import {
-  to = aws_api_gateway_resource.mp_rest_import
-  id = "e2h43o5aje/94o61z"
-}
-
-import {
-  to = aws_api_gateway_method.mp_rest_import_options
-  id = "e2h43o5aje/94o61z/OPTIONS"
-}
-
-import {
-  to = aws_api_gateway_integration.mp_rest_import_options
-  id = "e2h43o5aje/94o61z/OPTIONS"
-}
-
-import {
-  to = aws_api_gateway_method_response.mp_rest_import_options_200
-  id = "e2h43o5aje/94o61z/OPTIONS/200"
-}
-
-import {
-  to = aws_api_gateway_integration_response.mp_rest_import_options_200
-  id = "e2h43o5aje/94o61z/OPTIONS/200"
-}
-
-import {
-  to = aws_api_gateway_method.mp_rest_import_post
-  id = "e2h43o5aje/94o61z/POST"
-}
-
-import {
-  to = aws_api_gateway_integration.mp_rest_import_post
-  id = "e2h43o5aje/94o61z/POST"
-}
-
-import {
-  to = aws_api_gateway_resource.mp_rest_share
-  id = "e2h43o5aje/a4o679"
-}
-
-import {
-  to = aws_api_gateway_method.mp_rest_share_options
-  id = "e2h43o5aje/a4o679/OPTIONS"
-}
-
-import {
-  to = aws_api_gateway_integration.mp_rest_share_options
-  id = "e2h43o5aje/a4o679/OPTIONS"
-}
-
-import {
-  to = aws_api_gateway_method_response.mp_rest_share_options_200
-  id = "e2h43o5aje/a4o679/OPTIONS/200"
-}
-
-import {
-  to = aws_api_gateway_integration_response.mp_rest_share_options_200
-  id = "e2h43o5aje/a4o679/OPTIONS/200"
-}
-
-import {
-  to = aws_api_gateway_method.mp_rest_share_post
-  id = "e2h43o5aje/a4o679/POST"
-}
-
-import {
-  to = aws_api_gateway_integration.mp_rest_share_post
-  id = "e2h43o5aje/a4o679/POST"
-}
-
-import {
-  to = aws_api_gateway_resource.mp_rest_save
-  id = "e2h43o5aje/dbv9qk"
-}
-
-import {
-  to = aws_api_gateway_method.mp_rest_save_options
-  id = "e2h43o5aje/dbv9qk/OPTIONS"
-}
-
-import {
-  to = aws_api_gateway_integration.mp_rest_save_options
-  id = "e2h43o5aje/dbv9qk/OPTIONS"
-}
-
-import {
-  to = aws_api_gateway_method_response.mp_rest_save_options_200
-  id = "e2h43o5aje/dbv9qk/OPTIONS/200"
-}
-
-import {
-  to = aws_api_gateway_integration_response.mp_rest_save_options_200
-  id = "e2h43o5aje/dbv9qk/OPTIONS/200"
-}
-
-import {
-  to = aws_api_gateway_method.mp_rest_save_post
-  id = "e2h43o5aje/dbv9qk/POST"
-}
-
-import {
-  to = aws_api_gateway_integration.mp_rest_save_post
-  id = "e2h43o5aje/dbv9qk/POST"
-}
-
-import {
-  to = aws_api_gateway_resource.mp_rest_cook
-  id = "e2h43o5aje/ei8ozv"
-}
-
-import {
-  to = aws_api_gateway_method.mp_rest_cook_options
-  id = "e2h43o5aje/ei8ozv/OPTIONS"
-}
-
-import {
-  to = aws_api_gateway_integration.mp_rest_cook_options
-  id = "e2h43o5aje/ei8ozv/OPTIONS"
-}
-
-import {
-  to = aws_api_gateway_method_response.mp_rest_cook_options_200
-  id = "e2h43o5aje/ei8ozv/OPTIONS/200"
-}
-
-import {
-  to = aws_api_gateway_integration_response.mp_rest_cook_options_200
-  id = "e2h43o5aje/ei8ozv/OPTIONS/200"
-}
-
-import {
-  to = aws_api_gateway_method.mp_rest_cook_post
-  id = "e2h43o5aje/ei8ozv/POST"
-}
-
-import {
-  to = aws_api_gateway_integration.mp_rest_cook_post
-  id = "e2h43o5aje/ei8ozv/POST"
-}
-
-import {
-  to = aws_api_gateway_resource.mp_rest_plan
-  id = "e2h43o5aje/na23ga"
-}
-
-import {
-  to = aws_api_gateway_method.mp_rest_plan_options
-  id = "e2h43o5aje/na23ga/OPTIONS"
-}
-
-import {
-  to = aws_api_gateway_integration.mp_rest_plan_options
-  id = "e2h43o5aje/na23ga/OPTIONS"
-}
-
-import {
-  to = aws_api_gateway_method_response.mp_rest_plan_options_200
-  id = "e2h43o5aje/na23ga/OPTIONS/200"
-}
-
-import {
-  to = aws_api_gateway_integration_response.mp_rest_plan_options_200
-  id = "e2h43o5aje/na23ga/OPTIONS/200"
-}
-
-import {
-  to = aws_api_gateway_method.mp_rest_plan_post
-  id = "e2h43o5aje/na23ga/POST"
-}
-
-import {
-  to = aws_api_gateway_integration.mp_rest_plan_post
-  id = "e2h43o5aje/na23ga/POST"
-}
-
-import {
-  to = aws_api_gateway_deployment.mp_rest_prod
-  id = "e2h43o5aje/g8st8d"
-}
-
-import {
-  to = aws_api_gateway_stage.mp_rest_prod
-  id = "e2h43o5aje/prod"
-}
-
-import {
-  to = aws_apigatewayv2_api.mp_http
-  id = "dmtezcygeb"
-}
-
-import {
-  to = aws_apigatewayv2_stage.mp_http_default
-  id = "dmtezcygeb/$default"
-}
-
-import {
-  to = aws_apigatewayv2_integration.mp_http_kroger
-  id = "dmtezcygeb/s844kb4"
-}
-
-import {
-  to = aws_apigatewayv2_integration.mp_http_nutrition
-  id = "dmtezcygeb/10a0w9l"
-}
-
-import {
-  to = aws_apigatewayv2_route.mp_http_kroger_products
-  id = "dmtezcygeb/9qf3om5"
-}
-
-import {
-  to = aws_apigatewayv2_route.mp_http_kroger_locations
-  id = "dmtezcygeb/ty1i2kg"
-}
-
-import {
-  to = aws_apigatewayv2_route.mp_http_nutrition
-  id = "dmtezcygeb/ayc2fgc"
-}
-
-import {
-  to = aws_lambda_permission.apigw_kroger_http
-  id = "meal-planner-kroger/apigateway-invoke"
-}
-
-import {
-  to = aws_lambda_permission.apigw_nutrition_http
-  id = "meal-planner-nutrition/apigw-nutrition"
-}
-
-import {
-  to = aws_lambda_permission.apigw_groups
-  id = "meal-planner-groups/apigateway-groups-any"
-}
-
-import {
-  to = aws_lambda_permission.apigw_share
-  id = "meal-planner-share/apigateway-invoke"
-}
-
-import {
-  to = aws_lambda_permission.apigw_save_save
-  id = "MealPlannerSave/apigateway-invoke"
-}
-
-import {
-  to = aws_lambda_permission.apigw_save_import
-  id = "MealPlannerSave/apigateway-invoke-import"
-}
-
-import {
-  to = aws_lambda_permission.apigw_save_cook
-  id = "MealPlannerSave/apigw-cook"
-}
-
-import {
-  to = aws_lambda_permission.apigw_plan
-  id = "meal-planner-plan/apigateway-plan-v2"
+  to = aws_cloudwatch_log_group.save
+  id = "/aws/lambda/MealPlannerSave"
 }
